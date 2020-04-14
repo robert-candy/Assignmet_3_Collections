dec8b6adf756703425f6b3e3a75197441d5ded62
Robert <candyrobertt@gmail.com>
2020-04-15 01:27:54 +0300

    bu bizim ilk commitimizdir

--- assign_3_numbers.py
+++ assign_3_numbers.py
@@ -1,1 +1,9 @@
 # This is the first line of our numbers assignment program
+
+numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3]
+new_list = ['aaa', 'bbb', 'ccc', 555, 222, 999]
+
+for i in range(0,6):
+    print("new listin", i,". elemanı =", new_list[i])
+    
+print("program bitti")
